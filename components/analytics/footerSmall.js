774220d25e3e4e33a2a0d3f972d08e1352b6d1b3
subhendu17620 <subhendu17620@gmail.com>
2022-10-15 23:18:17 +0530

    auth, footer fixed

--- components/analytics/footerSmall.js
+++ components/analytics/footerSmall.js
@@ -1,4 +1,10 @@
-import React from 'react';
+import { useState, useCallback } from 'react';
+
+export default function useCounter() {
+    const [count, setCount] = useState(0);
+    const increment = useCallback(() => setCount((x) => x + 1), []);
+    return { count, increment };
+}
 
 export default function FooterSmall(props: any) {
     return (
