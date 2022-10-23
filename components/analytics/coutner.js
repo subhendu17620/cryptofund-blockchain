3ad11ef95926a3c0c46cddac3a20ddf1b7c554b2
sayan-maji <98383244+sayan-maji@users.noreply.github.com>
2022-10-23 10:10:45 +0530

    Update coutner.js

--- components/analytics/coutner.js
+++ components/analytics/coutner.js
@@ -1,7 +1,5 @@
 import { useState, useCallback } from 'react';
 
-export default function useCounter() {
-    const [count, setCount] = useState(0);
-    const increment = useCallback(() => setCount((x) => x + 1), []);
+export default function useCounter() {  const increment = useCallback(() => setCount((x) => x + 1), []);
     return { count, increment };
 }
